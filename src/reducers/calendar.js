84701bbf0f1f06145926b61a6c3363af73993c02
linjiang82 <linjiang82@gmail.com>
2018-08-16 10:52:57 +1000

    fixed the year when month meets the Jan or Dec.

--- src/reducers/calendar.js
+++ src/reducers/calendar.js
@@ -8,12 +8,24 @@ let init = {
 export default (state=init,action) => {
   switch(action.type){
     case 'previous':
+    if(state.month===0)
+    return state={
+      ...state,
+      month:11,
+      year:state.year-1
+    }
     return state={
       ...state,
       month:state.month-1
     }
     break;
     case 'next':
+    if(state.month===11)
+    return state={
+      ...state,
+      month:0,
+      year:state.year+1
+    }
     return state={
       ...state,
       month:state.month+1
